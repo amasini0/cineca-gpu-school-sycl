5fdc3be555f34c871d1db8b4c5484055cbf36b85
amasini0 <a.masini@cineca.it>
2024-07-12 12:46:08 +0200

    Fix solution with buffer

--- exercise/src/solutions/heat2d_sycl_buf.cpp
+++ exercise/src/solutions/heat2d_sycl_buf.cpp
@@ -33,7 +33,7 @@ int main(const int argc, const char** argv) {
     sycl::queue device_queue;
     
     // Print start message
-    std::cout << "2D heat equation solver (SYCL version)\n";
+    std::cout << "2D heat equation solver (sycl version)\n";
     std::cout << "Binary name: " << argv[0] << "\n";
     std::cout << "Device name: " << device_queue.get_device()
                                                 .get_info<sycl::info::device::name>() << "\n";
@@ -48,17 +48,17 @@ int main(const int argc, const char** argv) {
     std::vector<float> field_np1(num_elements);
     
     // Set initial conditions: disk of radius width/6
-    constexpr float radius2 = (nx / 6.) * (nx / 6.);
+    constexpr float radius2 = (nx/6.0) * (nx/6.0);
     for (int i = 0; i < nx; ++i) {
         for (int j = 0; j < ny; ++j) {
             const int linear_idx = get_linear_index(i, j, nx);
 
             // Distance of point i,j from center of domain
-            const int ds2 = (i - nx/2)*(i - nx/2) + (j - ny/2)*(j - ny/2);
+            const float ds2 = (i - nx/2)*(i - nx/2) + (j - ny/2)*(j - ny/2);
 
             // If distance is less then radius assign higher value
             if (ds2 < radius2) {
-                field_n[linear_idx] = 65.;
+                field_n[linear_idx] = 65.0;
             } else {
                 field_n[linear_idx] = 5.0;
             }
@@ -67,26 +67,31 @@ int main(const int argc, const char** argv) {
     
     // Copy field_n to field_np1
     std::copy(field_n.cbegin(), field_n.cend(), field_np1.begin());
-    
-    // Create buffers from vectors
-    sycl::buffer buff_n  (field_n  );
-    sycl::buffer buff_np1(field_np1);
-    
+
     // Start timer
     std::chrono::high_resolution_clock clock;
     const auto start = clock.now();
 
+    // Create buffers from host vectors
+    sycl::buffer buff_n  (field_n  );
+    sycl::buffer buff_np1(field_np1);
+
+    // Kernel launch parameters
+    sycl::range<2> total(nx, ny);
+    sycl::range<2> group(16, 16);
+    
+
     // Main loop
     for (int n = 0; n < num_steps; ++n) {
+        // Update field (in kernel)
         device_queue.submit([&](sycl::handler& h) {
+            // Declare device accessors
             sycl::accessor acc_n  (buff_n,   h);
             sycl::accessor acc_np1(buff_np1, h);
+
+            // Swap on alternating iterations
             if (n % 2 == 1) acc_n.swap(acc_np1);
 
-            sycl::range<2> total(nx, ny);
-            sycl::range<2> group(16, 16);
- 
-            
             h.parallel_for(sycl::nd_range(total, group), 
                 [=](auto nd_item) {
                     int i = nd_item.get_global_id(0);
@@ -110,7 +115,7 @@ int main(const int argc, const char** argv) {
                         acc_np1[linear_index] = field_ij + a * dt * (field_xx_ij + field_yy_ij); 
                     }
                 });
-        }).wait();
+        }).wait(); // Wait for kernel completion
     }
 
     // Compute elapsed time
@@ -136,6 +141,7 @@ int main(const int argc, const char** argv) {
     out_file.write((char*) &dt, sizeof(int));
     // Write field values at last timestep 
     {
+        // Use host accessor to safely access buffer memory
         sycl::host_accessor h_acc_n(buff_n, sycl::read_only);
         out_file.write((char*)&h_acc_n[0], h_acc_n.byte_size());
     } 
